0200eaa4972d84a8004272396145388ff348c029
popovycj <marian.popovych.ua@gmail.com>
2023-02-06 22:30:47 +0200

    Added average_rating field to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   has_and_belongs_to_many :categories
 
   validates :title, presence: true
+  validates :average_rating, presence: true, numericality: { in: 0..10 }
 
-  self.per_page = 3
+  self.per_page = 6
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_091607) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_192735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_091607) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "average_rating", default: 0.0
   end
 
   create_table "ratings", force: :cascade do |t|
