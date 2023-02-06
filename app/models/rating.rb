70164c9e98779d7312c4fd77ffcb65fc164a50dc
popovycj <marian.popovych.ua@gmail.com>
2023-02-06 22:33:18 +0200

    Changed Rating model so that it recalculates average rating of movie after creating; Updated seeds

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,12 @@ class Rating < ApplicationRecord
 
   validates :score, presence: true
   validates :score, numericality: { only_integer: true, in: 1..10 }
+
+  after_create :recalculate_movie_average_rating
+
+  private
+
+  def recalculate_movie_average_rating
+    movie.update(average_rating: movie.ratings.average(:score).to_f.round(1))
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,43 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-# Create categories
-categories = [
-  { name: "Action" },
-  { name: "Comedy" },
-  { name: "Drama" },
-  { name: "Thriller" },
-  { name: "Romance" }
-]
+# categories.rb
+Category.create!(name: "Action")
+Category.create!(name: "Comedy")
+Category.create!(name: "Drama")
+Category.create!(name: "Thriller")
 
-categories.each do |category|
-  Category.create!(category)
-end
+# movies.rb
+m1 = Movie.create!(title: "The Dark Knight")
+m2 = Movie.create!(title: "The Hangover")
+m3 = Movie.create!(title: "The Shawshank Redemption")
+m4 = Movie.create!(title: "The Silence of the Lambs")
+m5 = Movie.create!(title: "Inception")
+m6 = Movie.create!(title: "The Godfather")
+m7 = Movie.create!(title: "Pulp Fiction")
+m8 = Movie.create!(title: "The Matrix")
 
-# Create movies
-movies = [
-  { title: "The Avengers", categories: [Category.find_by(name: "Action"), Category.find_by(name: "Thriller")] },
-  { title: "The Hangover", categories: [Category.find_by(name: "Comedy")] },
-  { title: "The Shawshank Redemption", categories: [Category.find_by(name: "Drama")] },
-  { title: "The Notebook", categories: [Category.find_by(name: "Romance"), Category.find_by(name: "Drama")] },
-  { title: "The Dark Knight", categories: [Category.find_by(name: "Action"), Category.find_by(name: "Thriller")] }
-]
+m1.categories << Category.find_by(name: "Action")
+m1.categories << Category.find_by(name: "Thriller")
+m2.categories << Category.find_by(name: "Comedy")
+m3.categories << Category.find_by(name: "Drama")
+m4.categories << Category.find_by(name: "Thriller")
+m5.categories << Category.find_by(name: "Action")
+m5.categories << Category.find_by(name: "Thriller")
+m6.categories << Category.find_by(name: "Drama")
+m7.categories << Category.find_by(name: "Thriller")
+m7.categories << Category.find_by(name: "Comedy")
+m8.categories << Category.find_by(name: "Action")
+m8.categories << Category.find_by(name: "Thriller")
 
-movies.each do |movie|
-  new_movie = Movie.new(title: movie[:title])
-  new_movie.categories = movie[:categories]
-  new_movie.save!
-end
+# users.rb
+u1 = User.create!(email: "user1@example.com", password: "password", role: "user")
+u2 = User.create!(email: "user2@example.com", password: "password", role: "user")
+u3 = User.create!(email: "admin@example.com", password: "password", role: "admin")
+
+# ratings.rb
+Rating.create!(user: u1, movie: m1, score: 9)
+Rating.create!(user: u2, movie: m1, score: 8)
+Rating.create!(user: u1, movie: m2, score: 7)
+Rating.create!(user: u2, movie: m2, score: 8)
+Rating.create!(user: u3, movie: m3, score: 10)
