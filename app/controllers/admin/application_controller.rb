65be8dcdef3ec8fedae25af0e085baa33e96fc3f
popovycj <marian.popovych.ua@gmail.com>
2023-02-12 21:58:15 +0200

    Fixed AdminApplicationController

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,6 +4,6 @@ class Admin::ApplicationController < ApplicationController
   private
 
   def authenticate_admin!
-    redirect_to root_path, alert: "You are not authorized to access this page." unless current_user.admin?
+    redirect_to root_path, alert: "You are not authorized to access this page." unless current_user&.admin?
   end
 end
